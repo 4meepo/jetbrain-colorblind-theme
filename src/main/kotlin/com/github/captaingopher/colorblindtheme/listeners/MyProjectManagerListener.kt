f8f6cd0ae482a8ed760cb4ce4db3287647aa721b
GitHub Action <action@github.com>
2022-04-23 03:24:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/captaingopher/colorblindtheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/captaingopher/colorblindtheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.captaingopher.colorblindtheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.captaingopher.colorblindtheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/captaingopher/colorblindtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.captaingopher.colorblindtheme.services
+
+import com.github.captaingopher.colorblindtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
